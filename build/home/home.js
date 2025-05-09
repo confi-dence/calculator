740d2421cbd8d1565e13d0f99ceaf12a25bf018e
Confidence <confi2s7@gmail.com>
2025-05-09 20:15:36 +0100

    made some changes

--- build/home/home.js
+++ build/home/home.js
@@ -1,29 +1,28 @@
 const img = document.getElementById('img'),
     over = document.getElementById('over'),
-    header = document.getElementById('header')
+    header = document.getElementById('header'),
+    link = document.getElementById('link'),
+    removeContent = document.getElementById('removeContent')
+
     
 
 
     let hasRotated = false
 function image() {
-    // img.classList.add('imgs')
     over.style.display = 'block'
-    // img.style.opacity= '0.2'
     header.innerText= "Thanks for clicking"
-if(!hasRotated){
+    if(!hasRotated){
     header.classList.toggle('rotated')
       hasRotated = true
 }    
 }
-
-{/* <script>
-    const box = document.getElementById('rotatableBox');
-    let hasRotated = false;
-
-    box.addEventListener('click', function() {
-        if (!hasRotated) {
-            box.style.transform = 'rotate(360deg)';
-            hasRotated = true;
-        }
-    });
-</script> */}
+function loading(event){
+    removeContent.style.display = 'none'
+    event.preventDefault();
+    header.innerText = "loading........"
+  header.classList.remove('rotated')
+    setTimeout(()=>{
+            window.location.href = link.href
+        }, 10000)
+}
+link.addEventListener('click', loading)
