a36bf0ae1321c8857caa5ada8c363df41cc61b86
Confidence <confi2s7@gmail.com>
2025-05-13 11:38:11 +0100

    did some js

--- build/calculator/calculator.js
+++ build/calculator/calculator.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById('modal'),
 closed = document.getElementById('close'),
-openMoa = document.getElementById('openMoa')
+openMoa = document.getElementById('openMoa'),
+clear = document.getElementById('clear'),
+overResult = document.getElementById('overResult')
 
 closed.addEventListener('click', function () {
     modal.style.display = 'none'
@@ -8,3 +10,37 @@ closed.addEventListener('click', function () {
 openMoa.addEventListener('click', function (params) {
     modal.style.display = 'flex'
 })
+
+function showValue(number) {
+    if(score.innerText=== '0' ){
+        if (isoperatoe(number)) {
+            return;
+        }else{
+        score.innerText = number
+        }
+    }else{
+score.innerText += number
+    }
+}
+function  isoperatoe(char) {
+    return['+', '-0', '*' ,'/', '%','**'  ].includes(char)
+}
+function clearNumber() { 
+    score.innerText = '0';
+}
+clear.addEventListener('click', clearNumber)
+
+function results() {
+    try {
+        score.innerText = eval(score.innerText)
+    } catch (error) {
+        if(score.innerText === 'Syntax Error'){
+            score.innerText = '0'
+        }else{
+            score.innerText = 'Syntax Error'
+        }
+    }
+}
+overResult.addEventListener('click', results)
+
+
